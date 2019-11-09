6a648f40e7559f2f3d850bb09955aa0e59a43e1e
fanlushuai <fanlushuai@outlook.com>
2019-11-09 23:16:57 +0800

    feature(微信发送错误日志): 使得发送消息的内容更加丰富。包含错误日志，错误日志产生的className，以及top10栈信息。

--- src/main/java/name/auh/util/WeChatAppender.java
+++ src/main/java/name/auh/util/WeChatAppender.java
@@ -24,14 +24,42 @@ public class WeChatAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
         if (!isStarted()) {
             return;
         }
+
         try {
             if (LimitUtil.overRate(event.getMessage(), 1, 5, TimeUnit.MINUTES)) {
                 return;
             }
 
-            wxCpService.messageSend(WxCpMessage.TEXT().agentId(0).toUser("@all").content(event.getMessage()).build());
+            String fatLog = parseFatLogForQYWX(event);
+
+            wxCpService.messageSend(WxCpMessage.TEXT().agentId(0).toUser("@all").content(fatLog).build());
+
         } catch (WxErrorException e) {
             e.printStackTrace();
         }
     }
+
+    private String parseFatLogForQYWX(ILoggingEvent event) {
+        if (event == null) {
+            return "";
+        }
+
+        //日志事件的最终消息，需要格式化一下。位置：org.slf4j.helpers.MessageFormatter.arrayFormat(java.lang.String, java.lang.Object[], java.lang.Throwable)
+        //所以使用的时候 event.getMessage() 简单不完整的消息;  event.getFormattedMessage() 带参数的完整消息
+        //如果需要打印具体的栈信息，也是可以的
+        StringBuilder fatLog = new StringBuilder("\uD83D\uDE2D[MSG]\uD83D\uDE2D\n").append(event.getFormattedMessage());
+        if (event.getThrowableProxy() == null) {
+            return fatLog.toString();
+        }
+
+        //小坑，本来想发送markdown消息。但是微信企业版app才支持这种消息格式。使用普通微信接受企业号消息是不支持的。尴尬。
+        //无奈，只能使用emoji来替代格式了。
+        fatLog.append("\n\n").append("\uD83D\uDE2C[MSG_CLASS]\uD83D\uDE2C\n").append(event.getThrowableProxy().getClassName());
+        fatLog.append("\n\n").append("\uD83D\uDC40[Stack-Top10]\uD83D\uDC40");
+        for (int i = 0, forLength = (Math.min(event.getThrowableProxy().getStackTraceElementProxyArray().length, 10)); i < forLength; i++) {
+            fatLog.append("\n").append(event.getThrowableProxy().getStackTraceElementProxyArray()[i].getSTEAsString());
+        }
+        return fatLog.toString();
+    }
+
 }
